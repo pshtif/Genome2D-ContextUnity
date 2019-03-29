a415e60644adfdd1f3da88d20deb312531baa48c
sHTiF <pshtif@gmail.com>
2019-03-29 01:04:11 +0100

    update

--- plugin/src/GNativeUnityAsset.cs
+++ plugin/src/GNativeUnityAsset.cs
@@ -1,7 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
 namespace UnityTestLibrary
 {
     public class GNativeUnityAsset
     {
+        protected MonoBehaviour _wrapper;
+        protected Action<string> _loaded;
         
+        public GNativeUnityAsset(MonoBehaviour p_wrapper, Action<string> p_loaded)
+        {
+            _wrapper = p_wrapper;
+            _loaded = p_loaded;
+        }
+        
+        public void Load(string p_url)
+        {
+            _wrapper.StartCoroutine(GetTexture(p_url));
+        }
+        
+        IEnumerator GetTexture(string p_url)
+        {
+            using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(p_url))
+            {
+                yield return uwr.SendWebRequest();
+
+                if (uwr.isNetworkError || uwr.isHttpError)
+                {
+                    Debug.Log(uwr.error);
+                }
+                else
+                {
+                    Debug.Log("Loaded");
+                    _loaded(p_url);
+                    // Get downloaded asset bundle
+                    //texture = DownloadHandlerTexture.GetContent(uwr);
+                    MeshRenderer mr = _wrapper.GetComponentInChildren<MeshRenderer>();
+                    mr.sharedMaterials[0].mainTexture = DownloadHandlerTexture.GetContent(uwr);
+                }
+            }
+        }
     }
 }
--- src/com/genome2d/context/GUnityContext.hx
+++ src/com/genome2d/context/GUnityContext.hx
@@ -9,13 +9,19 @@ class GUnityContext extends MonoBehaviour {
 	function Start() {
 		Debug.Log('Initialized...');		
 
-		var utils:GNativeUnityContext = new GNativeUnityContext(this);
-        utils.AddValues(2, 3);
-        Debug.Log("2 + 3 = " + utils.c);
-		utils.LoadTexture("http://i.imgur.com/oIaHNEG.jpg");
+		var test:GNativeUnityAsset = new GNativeUnityAsset(this, call);
+
+		//var utils:GNativeUnityContext = new GNativeUnityContext(this);
+        //utils.AddValues(2, 3);
+        //Debug.Log("2 + 3 = " + utils.c);
+		test.Load("http://i.imgur.com/oIaHNEG.jpg");
 	}
 
 	function Update () {
         //Debug.Log(GNativeUnityContext.GenerateRandom(0, 100));
     }
+
+	function call(p_url:String) {
+		Debug.Log("Fuck "+p_url);
+	}
 }
