d3f10e177fbf1243ad5b6a2ba0283b26b9ced137
Yağız AYER <yagizayeryy@gmail.com>
2025-09-10 10:14:08 +0300

    Added simulated touches to GOverridableUnityInput and set it as default unityInputType

--- src/com/genome2d/input/GOverridableUnityInput.hx
+++ src/com/genome2d/input/GOverridableUnityInput.hx
@@ -7,6 +7,45 @@ import unityengine.*;
 
 class GOverridableUnityInput implements IGUnityInput {
 
+    // Simulate Input Logic
+    // Simulate Input Logic is manipulated via reflection, don't change signatures
+    public static var simulatedTouches:List<Touch> = new List<Touch>();
+
+    public static var touches(get, null):Array<Touch>;
+
+    static function get_touch_count():Int {
+        if (simulatedTouches.isEmpty()) {
+            return GNativeUnityOverridableInput.BaseInput.touchCount;
+        }
+        return 1;
+    }
+
+    static function get_touches():Array<Touch> {
+        var touchCount = get_touch_count();
+        var tempTouches = new Array<Touch>();
+        for (i in 0...touchCount) {
+            tempTouches.push(get_touch(i));
+        }
+        return tempTouches;
+    }
+
+    public static function get_touch(id:Int, ?eatTouch:Bool = true):Touch {
+        if (simulatedTouches.isEmpty()) {
+            return GNativeUnityOverridableInput.BaseInput.GetTouch(id);
+        }
+
+        // currently two finger gestures are not supported for simulating
+        if (id >= 1) {
+            return GNativeUnityOverridableInput.BaseInput.GetTouch(id);
+        }
+
+        if (eatTouch) {
+            return simulatedTouches.pop(); // remove first
+        }
+        return simulatedTouches.first(); // peek
+    }
+    // End Simulate Input Logic
+
     public function new() {
     }
 
@@ -15,36 +54,43 @@ class GOverridableUnityInput implements IGUnityInput {
     }
 
     public function getTouchCount():Int {
-        return GNativeUnityOverridableInput.BaseInput.touchCount;
+        return get_touch_count();
     }
 
     public function getTouch(index:Int):Touch {
-        return GNativeUnityOverridableInput.BaseInput.GetTouch(index);
+        return get_touch(index, false);
     }
 
     public function getTouches():NativeArray<Touch> {
-        var touches = new NativeArray<Touch>(getTouchCount());
-        for (i in 0...touches.length) {
-            touches[i] = getTouch(i);
+        var arr = touches;
+        var nativeArr = new NativeArray<Touch>(arr.length);
+        for (i in 0...arr.length) {
+            nativeArr[i] = arr[i];
         }
-
-        return touches;
+        return nativeArr;
     }
 
     public function getMouseButtonDown(button:Int):Bool {
-        return GNativeUnityOverridableInput.BaseInput.GetMouseButtonDown(button);
+        return (get_touch_count() > 0 && get_touch(0, false).phase == TouchPhase.Began) || 
+        GNativeUnityOverridableInput.BaseInput.GetMouseButtonDown(button);
     }
 
     public function getMouseButtonUp(button:Int):Bool {
-        return GNativeUnityOverridableInput.BaseInput.GetMouseButtonUp(button);
+        return (get_touch_count() > 0 && get_touch(0, false).phase == TouchPhase.Ended) || 
+        GNativeUnityOverridableInput.BaseInput.GetMouseButtonUp(button);
     }
 
     public function getMouseButton(button:Int):Bool {
-        return GNativeUnityOverridableInput.BaseInput.GetMouseButton(button);
+        return (get_touch_count() > 0 && (
+                get_touch(0, false).phase == TouchPhase.Began || 
+                get_touch(0, false).phase == TouchPhase.Moved || 
+                get_touch(0, false).phase == TouchPhase.Stationary)
+            ) || 
+        GNativeUnityOverridableInput.BaseInput.GetMouseButton(button);
     }
 
     public function getMousePosition():Vector2 {
-        return GNativeUnityOverridableInput.BaseInput.mousePosition;
+        return (get_touch_count() > 0) ? get_touch(0, false).position : GNativeUnityOverridableInput.BaseInput.mousePosition;
     }
 
     public function getMouseScrollDelta():Vector2 {
